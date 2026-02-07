de7542c0fb3298521bac62c709b896e39257d0f7
monday22 <mondaynovember22@gmail.com>
2026-02-07 21:47:35 +0000

    Section 7

---
+++ yinying/src/app/shared/components/SelectInput.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import { useController, type FieldValues, type UseControllerProps } from "react-hook-form";
+type Props<T extends FieldValues> = {
+    type?: string;
+    label: string;
+    options: { key: string, label: string }[];
+} & UseControllerProps<T>;
+export default function SelectInput<T extends FieldValues>(props: Props<T>) {
+    const { field, fieldState } = useController({ ...props });
+    return (
+        <label className="floating-label text-left w-full">
+            <span>{props.label}</span>
+            <select
+                {...field}
+                value={field.value || `Select a ${props.label}`}
+
+                className={clsx('select select-lg w-full', {
+                    'input-error': !!fieldState.error,
+                    'input-success': !fieldState.error && fieldState.isDirty
+                })}
+            > 
+                <option disabled>Select a {props.label}</option>
+                {props.options.map(option => (
+                    <option key={option.key} value={option.key}>{option.label}</option>
+                ))}
+            </select>
+            {fieldState.error && (
+                <div className="validator-hint text-red-700">{fieldState.error.message}</div>
+            )}
+        </label>
+    )
+}   
